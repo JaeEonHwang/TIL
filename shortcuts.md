66f2a6fd34e070f4223f9c3b290a292440298944
JaeeonHwang <hju753@naver.com>
2023-07-19 17:26:45 +0900

    0719 update

--- shortcuts.md
+++ shortcuts.md
@@ -4,3 +4,4 @@
 * ctrl + insert : 복사(git bash)
 * shift + insert : 붙여넣기(git bash)
 * ctrl + ~ : VS code 터미널 열기
+* ctrl + c : 무한루프 빠져나오기
--- test.py
+++ test.py
@@ -1,3 +1,26 @@
-result = [x*y for x in range(2,10) for y in range(1,10)]
+def create_user(name, age, address):
+    print(f'{name}님 환영합니다!')
+    user_info_name, user_info_age, user_info_address = name, age, address
+    user_info['name'] = user_info_name
+    user_info['age'] = user_info_age
+    user_info['address'] = user_info_address
+    people_list.append(user_info)
+    return people_list
 
-print(result)
+
+name = ['김시습', '허균', '남영로', '임제', '박지원']
+age = [20, 16, 52, 36, 60]
+address = ['서울', '강릉', '조선', '나주', '한성부']
+
+people = list(zip(name,age,address))
+user_info = {}
+people_list =[]
+
+# for person in people:
+#     name, age, address = person
+#     create_user(name, age, address)
+
+# print(people_list)
+
+print(people)
+print(*people)
