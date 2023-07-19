436b5d70369d8060f5e960bb4d477f4a2ed10455
JaeeonHwang <hju753@naver.com>
2023-07-19 18:00:28 +0900

    update

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-def create_user(name, age, address):
+def create_user(person):
+    name, age, address = person
+    user_info = {}
     print(f'{name}님 환영합니다!')
-    user_info_name, user_info_age, user_info_address = name, age, address
-    user_info['name'] = user_info_name
-    user_info['age'] = user_info_age
-    user_info['address'] = user_info_address
+    user_info['name'] = name
+    user_info['age'] = age
+    user_info['address'] = address
     people_list.append(user_info)
     return people_list
-
 
 name = ['김시습', '허균', '남영로', '임제', '박지원']
 age = [20, 16, 52, 36, 60]
 address = ['서울', '강릉', '조선', '나주', '한성부']
 
 people = list(zip(name,age,address))
-user_info = {}
+
 people_list =[]
 
 # for person in people:
@@ -22,5 +22,11 @@ people_list =[]
 
 # print(people_list)
 
-print(people)
-print(*people)
+# print(*people[0])
+# print(people)
+
+#print(list(map(create_user,people[i])))
+
+list(map(create_user,people))
+
+print(people_list)
