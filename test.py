77b80d57ef62f754b9f67fe0f77efaa883b5d81d
JaeEon Hwang <hju753@naver.com>
2023-07-22 18:42:53 +0900

    0722 update

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-import sys
+fruits = ['apple', 'banana', 'cherry']
 
-
-a = int(input())
-
-q, w, e, r, t, y = map(int,input().split())
-
-print(a)
-print(w)
+for index in enumerate(fruits):
+    print(index)
