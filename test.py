0930a62db374523fcb4f890969b8f3f286b0c5db
JaeEonHwang <hju753@naver.com>
2023-07-24 14:42:45 +0900

    0724 update

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
-def reverse_string(string):
-    return string.reverse()
+def capitalize_words(some_str):
+    new_list = list(some_str.split())
+    second_new_list = []
+    for word in new_list:
+        new_word = word[0].upper() + word[1:]
+        second_new_list.append(new_word)
+    return ' '.join(second_new_list)
 
-result = reverse_string("Hello, World!")
+result = capitalize_words("hello, world!")
 print(result)
+
+
+
