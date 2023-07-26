c19e8c4f3fd3b65d81b9199352f68c3690565ed2
JaeEonHwang <hju753@naver.com>
2023-07-26 15:20:49 +0900

    0726 update

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-print("life" "is" "too short",sep='@',end = '\t')
-print("life"+"is"+"too short",sep='@',end = '\t')
-print('life','is','too short',sep='@',end = '\t')
+class Shape:
+   
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def __str__(self):
+        return f'Shape: width={self.width}, height={self.height}'
+
+shape1 = Shape(5, 3)
+print(shape1)
