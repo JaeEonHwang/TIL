715ac03a6ad562f973e45fc858b433518de16d35
kdh <dohoon122@gmail.com>
2023-08-01 17:56:32 +0900

    0801 update

--- Algorithm/SWEA/SWEA4813.py
+++ Algorithm/SWEA/SWEA4813.py
@@ -2,18 +2,18 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 T = int(input())
-for i in range(1,T+1):
-    K,N,M = list(map(int,input().split()))
-    numberM = list(map(int,input().split()))
+for i in range(1, T+1):
+    K, N, M = list(map(int, input().split()))
+    numberM = list(map(int, input().split()))
     charging = 0
     busstop = K
     previous_busstop = 0
-    while busstop < 10:
+    while busstop < N:
         while busstop not in numberM:
             busstop -= 1
-            if busstop == previous_busstop:
-                print(f'#{i} 0')
-                break
+        if busstop == previous_busstop:
+            charging = 0
+            break
         previous_busstop = busstop
         busstop += K
         charging += 1
