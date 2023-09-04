3055453b28e4987a6bd08643dc11161cafbdcff7
JaeEon Hwang <hju753@naver.com>
2023-09-05 00:45:04 +0900

    baseball done

--- Algorithm/Baekjoon/BJ12100.py
+++ Algorithm/Baekjoon/BJ12100.py
@@ -3,44 +3,43 @@ sys.stdin = open('input.txt', 'r')
 
 from copy import deepcopy
 
-
+# 2048 게임하는 함수, k는 이동 횟수
 def game2048(k):
-    global cnt
     global max_v
-    if k == 5:
-        for row in range(N):
-            if max(arrs[5][row]) > max_v:
+    if k == 5:                              # 5번 다 움직였으면
+        for row in range(N):                # 최댓값 찾아서
+            if max(arrs[5][row]) > max_v:   # 기존 최댓값이랑 비교
                 max_v = max(arrs[5][row])
         return
 
-    for i in range(4):
-        if arrs[k+1] != arrs[k]:
-            arrs[k + 1] = deepcopy(arrs[k])
-            arr = arrs[k + 1]
+    for i in range(4):     # 동서남북 네 방향에 대해서 검사
+        if arrs[k+1] != arrs[k]:       #주어진 board를 수정하는 대신 복사한 arr를 수정할 예정, visited처럼 기록을 남겨놓기 위함
+            arrs[k + 1] = deepcopy(arrs[k])   # k번째면 arr[k+1]에 있는 테이블을 바꿈
+            arr = arrs[k + 1]                 # 만약 바꿔야 할 arr[k+1]이 이전에 바뀌었던 arr[k]와 다르면 새로 deepcopy
         else:
-            arr = arrs[k + 1]
-        if i == 0:
+            arr = arrs[k + 1]                 # 같으면 그대로 진행
+        if i == 0:                            # 아마 동쪽으로 이동...?
             for row in range(N):
-                for col in range(N-1, 0, -1):
+                for col in range(N-1, 0, -1):   # 해당 열에 0을 서쪽으로 옮기는 과정
                     if arr[row][:arr[row].count(0)] == [0] * arr[row].count(0):
-                        break
+                        break         # 해당 열에 0이 모두 서쪽에 몰려있으면 break
                     temp = 0
-                    while arr[row][col] == 0:
+                    while arr[row][col] == 0:   # 해당 열에서 동쪽에 있는 칸부터 검사하면서 만약 0이면
                         temp += 1
                         if temp > N:
-                            break
-                        for j in range(col):
+                            break               # 해당 열의 모든 값이 0이면 break
+                        for j in range(col):    # 해당 열의 모든 값을 동쪽으로 한 칸씩 옮김
                             arr[row][col - j] = arr[row][col - j - 1]
-                        arr[row][0] = 0
-                for col in range(N - 1, 0, -1):
-                    if arr[row][col] == arr[row][col-1] and arr[row][col] != 0:
-                        arr[row][col] *= 2
+                        arr[row][0] = 0         # 제일 서쪽에 있는 값은 0
+                for col in range(N - 1, 0, -1):    # 숫자를 동쪽으로 옮기면서 더하는 과정 (동쪽에 있는 숫자부터 확인)
+                    if arr[row][col] == arr[row][col-1] and arr[row][col] != 0:  # 만약 왼쪽에 있는 숫자와 같은데 0이 아니면
+                        arr[row][col] *= 2  # 해당 숫자는 2 곱하고
                         for j in range(1, col):
-                            arr[row][col - j] = arr[row][col - j - 1]
+                            arr[row][col - j] = arr[row][col - j - 1]  # 다른 숫자는 동쪽으로 한 칸씩 옮김
                             if arr[row][col - j - 1] == 0:
-                                break
+                                break         # 옮기다가 0을 만나면 그만 옮기기
                         arr[row][0] = 0
-        elif i == 1:
+        elif i == 1:           # 서쪽으로 이동, 방향만 다르고 같은 원리
             for row in range(N):
                 for col in range(N - 1):
                     if arr[row][-arr[row].count(0):] == [0] * arr[row].count(0):
@@ -61,8 +60,8 @@ def game2048(k):
                             if arr[row][col + j + 1] == 0:
                                 break
                         arr[row][-1] = 0
-        elif i == 2:
-            for col in range(N):
+        elif i == 2:     # 남쪽, row/col 반대로 되어있지만 원리는 같음. 하지만 같은 열은 하나의 리스트에 있는 것이 아니기 때문에
+            for col in range(N):            # 코드는 약간 다름
                 for row in range(N - 1, 0, -1):
                     temp = 0
                     while arr[row][col] == 0:
@@ -80,7 +79,7 @@ def game2048(k):
                             if arr[row-j-1][col] == 0:
                                 break
                         arr[0][col] = 0
-        elif i == 3:
+        elif i == 3:    # 북쪽
             for col in range(N):
                 for row in range(N-1):
                     temp = 0
@@ -105,7 +104,7 @@ def game2048(k):
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
 max_v = 0
-arrs = [[]] * 6
-arrs[0] = board
-game2048(0)
+arrs = [[]] * 6   # 변경 전 2048 테이블을 저장하기 위함. arrs[k] -> k번 움직이고 나서 테이블 모양
+arrs[0] = board   # 초기 값은 주어진 테이블
+game2048(0)       # 0부터 시작해서 5가 되면 멈출 예정
 print(max_v)
--- Algorithm/Baekjoon/BJ17281.py
+++ Algorithm/Baekjoon/BJ17281.py
@@ -1,70 +1,85 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-import sys
+# 순열 생성 외부 라이브러리
 from itertools import permutations
-from copy import deepcopy
 
 
+# 각 이닝에 최대로 얻을 수 있는 점수를 대략적으로 계산해서 max_per_inning 리스트에 넣음
+# 나중에 순열 순환할 때 시간 줄일 수 있음
 def max_per_inning(arr):
-    arr = deepcopy(arr)
-    for i in range(N-1, -1, -1):
+    for i in range(N-1, -1, -1):   # 이닝은 뒤에서부터 계산
         score = 0
-        ai = arr[i]
-        if sum(ai):
-            ai.sort()
-            for j in range(9):
-                if ai[j] != 0:
-                    startidx = j
-                    break
-            cnt0 = 0
-            while cnt0 < 3:
-                if ai[startidx] == 0:
-                    cnt0 += 1
-                else:
-                    score = (score << ai[startidx]) + 2 ** (ai[startidx] - 1)
-                startidx = (startidx + 1) % 9
-            for k in bin(score)[-3:]:
-                if k == '1':
-                    score -= 1
+        for j in arr[i]:
+            if j != 0:            # 일단 안타 하나당 1점씩 추가
+                score += 1
+        if arr[i].count(0) == 1:    # 해당 이닝에 아웃당하는 사람이 1명이면 타순 사이클이 3번 돎.
+            score *= 3
+        elif arr[i].count(0) == 2:  # 해당 이닝에 아웃당하는 사람이 2명이면 타순 사이클이 2번 돎.
+            score *= 2
+        if max(arr[i]) == 3 or max(arr[i]) == 2:  # 해당 이닝에 제일 긴 장타가 2루타나 3루타면 적어도 1명 남는 주자가 있기 때문에
+            score -= 1                            # 1점 빼줌
+        elif max(arr[i]) == 1:                    # 해당 이닝에 1루타 밖에 없으면 주자가 3명 남아있음
+            score -= 3
+        if score < 0:        # 빼줬는데 음수면 득점은 0점
+            score = 0
         if i == N-1:
-            max_score[N-1] = score
+            max_score.insert(0, score)     # max_score[inning] = 해당 이닝부터 원하는 대로 타선을 바꿀 때 낼 수 있는 최대 점수
         else:
-            max_score[i] = score + max_score[i+1]
+            max_score.insert(0, score + max_score[0])
 
 
-N = int(sys.stdin.readline())
-table = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-max_score = [0] * N
+N = int(input())
+table = [list(map(int, input().split())) for _ in range(N)]
+max_score = []
 max_per_inning(table)
-lineups = permutations((1, 2, 3, 4, 5, 6, 7, 8), 8)
+players = (1, 2, 3, 4, 5, 6, 7, 8)    # 1번(인덱스 0번)은 4번타자 고정이기 때문에 8개 숫자로 순열 조합
+lineups = list(permutations(players, 8))
 max_v = 0
 for lineup in lineups:
-    if sum(table[0][1:]) and table[0][lineup[0]] == 0:
-        continue
-    if max_score[0] == max_v:
-        break
-    total = 0
+    total = 0   # 해당 라인업으로 얻는 점수
     lineup = list(lineup)
-    lineup.insert(3, 0)
+    lineup.insert(3, 0)    # 4번타순은 1번
     idx = 0
     for inning in range(N):
-        outcount = 0
-        inning_score = 0
-        while outcount < 3:
-            a = table[inning][lineup[idx]]
+        base = [0] * 4    # [1] = 1루, [2] = 2루, [3] = 3루, [0] = 홈(점수)
+        count0 = 0        # 아웃카운트
+        while count0 < 3:  # 아웃카운트 3개 될 때까지 이닝 진행
+            if table[inning][lineup[idx]] == 0:    # 0이면 아웃카운트 1 증가
+                count0 += 1
+            elif table[inning][lineup[idx]] == 1:   # 1루타면 한 칸씩 미룸
+                if base[3] == 1:
+                    base[0] += 1
+                base[3], base[2] = base[2], base[1]
+                base[1] = 1
+            elif table[inning][lineup[idx]] == 2:   # 2루타면 두 칸씩 미룸
+                if base[3] == 1:
+                    base[0] += 1
+                if base[2] == 1:
+                    base[0] += 1
+                base[3], base[2] = base[1], 1
+                base[1] = 0
+            elif table[inning][lineup[idx]] == 3:   # 3루타면 세 칸씩 미룸
+                if base[3] == 1:
+                    base[0] += 1
+                if base[2] == 1:
+                    base[0] += 1
+                if base[1] == 1:
+                    base[0] += 1
+                base[3], base[2], base[1] = 1, 0, 0
+            elif table[inning][lineup[idx]] == 4:  # 홈런이면 다 비우고 주자 + 1 만큼 점수 더하기
+                if base[3] == 1:
+                    base[0] += 1
+                if base[2] == 1:
+                    base[0] += 1
+                if base[1] == 1:
+                    base[0] += 1
+                base[0] += 1
+                base[3], base[2], base[1] = 0, 0, 0
             idx = (idx + 1) % 9
-            if a == 0:
-                outcount += 1
-                continue
-            inning_score = (inning_score << a) + 2**(a-1)
-            total += 1
-        a = bin(inning_score)[-3:]
-        for i in range(-3, 0):
-            if a[i] == '1':
-                total -= 1
-        if inning < N-1 and total + max_score[inning + 1] <= max_v:
-            break
+        total += base[0]      # base[0] = 해당 이닝에서 얻은 점수
+        if inning < N-1 and total + max_score[inning + 1] < max_v:
+            break     # 순열 순회 빨리 하기 위한 조건, 앞으로 이닝당 최고점수만큼 얻어도 max 값보다 작으면 해당 라인업 확인 그만
     if max_v < total:
         max_v = total
 print(max_v)
